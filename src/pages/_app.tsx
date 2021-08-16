e0f56b3b98976cfba2655e2f72b583764b190e39
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2021-08-16 16:07:44 +0900

    Add: config for Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import '../styles/global.css'
 import 'modern-css-reset/dist/reset.min.css'
+import { useRouter } from 'next/router'
+import * as gtag from '../libs/gtag'
 
-const BlogApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+const BlogApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  // Google Analyticsをページ遷移時にも対応させる
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
---
+++ src/pages/_document.tsx
@@ -0,0 +1,39 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../libs/gtag'
+
+class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          {/* Google Analytics */}
+          {GA_TRACKING_ID && (
+            <>
+              <script
+                async={true}
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });`,
+                }}
+              />
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
