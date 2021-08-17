2cc3729a02b6d45965626cd8f40f42587ef66e7b
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2021-08-17 15:15:35 +0900

    Add: fontawesome icon

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react'
 import css from 'styled-jsx/css'
 
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import config from '../../configs/config.json'
 import colors from '../../configs/colors.json'
 import FooterMenu from '../molecules/FooterMenu'
 
@@ -13,15 +16,23 @@ const styles = css`
     background-color: ${colors.main};
     margin: 0 auto;
   }
-  .footer_copyright {
+  .footer_copy_right {
     color: ${colors.white};
     text-align: center;
     padding: 5px 0 5px;
     background-color: ${colors.main};
   }
+  .footer_privacy_policy {
+    color: ${colors.white};
+    text-align: center;
+    background-color: ${colors.main};
+  }
+  .footer_privacy_policy a:hover {
+    text-decoration: underline;
+  }
 `
 
-const Footer: React.FC = ({ children }) => {
+const Footer: React.FC = () => {
   const items = [
     { name: 'About', link: '/about' },
     { name: 'GitHub', link: 'https://github.com/kentaro-fujita/' },
@@ -33,8 +44,19 @@ const Footer: React.FC = ({ children }) => {
       <div className="footer_menu_wrapper">
         <FooterMenu items={items} />
       </div>
-      <div className="footer_copyright">
-        <footer>{children}</footer>
+      <div className="footer_copy_right">
+        <span>{config.copyRight}</span>
+      </div>
+      <div className="footer_privacy_policy">
+        <span>This site uses </span>
+        <a href="https://policies.google.com/privacy">
+          <FontAwesomeIcon
+            icon={faExternalLinkAlt}
+            size="sm"
+            color={`${colors.white}`}
+          />
+          Google Analytics
+        </a>
       </div>
       <style jsx>{styles}</style>
     </Fragment>
