a45f86377af6ad3abe848039db16177ca4772358
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2021-09-08 00:39:49 +0900

    Update: remark

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import remark from 'remark'
+import { remark } from 'remark'
 import strip from 'strip-markdown'
 import config from '../configs/config.json'
 import { Post } from './models'
