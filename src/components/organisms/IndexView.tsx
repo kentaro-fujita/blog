42a03bdb59952287178740bbf9874fb6eafda99e
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2021-10-24 02:25:44 +0900

    Fix: build fail

--- src/components/organisms/IndexView.tsx
+++ src/components/organisms/IndexView.tsx
@@ -1,30 +1,7 @@
 import React, { Fragment } from 'react'
-import css from 'styled-jsx/css'
-import DigestList, { DigestListProps } from './DigestList'
 import Pagination, { PaginationProps } from './Pagination'
 import PostList, { PostListProps } from './PostList'
 import SideBar, { SideBarProps } from './SideBar'
-
-const styles = css`
-  .index_view {
-    width: 80%;
-    margin: 0 auto;
-  }
-  .index_view_main {
-    width: 80%;
-    float: left;
-  }
-  @media screen and (max-width: 640px) {
-    .index_view {
-      width: 100%;
-      margin: 0 auto;
-    }
-    .index_view_main {
-      width: 100%;
-      padding: 1rem;
-    }
-  }
-`
 
 // export type IndexViewProps = DigestListProps & PaginationProps & SideBarProps
 export type IndexViewProps = PostListProps
@@ -46,7 +23,6 @@ const IndexView: React.FC<IndexViewProps> = ({
         </div>
         <SideBar latestPosts={latestPosts} tags={tags} />
       </div> */}
-      {/* <style jsx>{styles}</style> */}
     </Fragment>
   )
 }
