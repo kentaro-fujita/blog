feae93b2ec1c663ef0843bc4d9a04135571be53c
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2021-11-15 01:37:07 +0900

    fix: reload bug

--- src/components/molecules/PostViewContent.tsx
+++ src/components/molecules/PostViewContent.tsx
@@ -13,56 +13,6 @@ export type PostViewContentProps = {
 }
 
 const PostViewContent: React.FC<PostViewContentProps> = ({ content }) => {
-  const H1: React.FC = ({ children }) => {
-    return (
-      <Title type="h1" className="text-center mb-4">
-        {children}
-      </Title>
-    )
-  }
-
-  const H2: React.FC = ({ children }) => {
-    return (
-      <Title type="h2" className="mb-2 mt-6">
-        {children}
-      </Title>
-    )
-  }
-
-  const H3: React.FC = ({ children }) => {
-    return (
-      <Title type="h3" className="mb-2 mt-4">
-        {children}
-      </Title>
-    )
-  }
-
-  // const CodeBlock: Components<code> = ({
-  //   inline,
-  //   className,
-  //   children,
-  //   ...props
-  // }) => {
-  //   const match = /language-(\w+)/.exec(className || '')
-  //   return !inline && match ? (
-  //     <SyntaxHighlighter style={dracula} language={match[1]} PreTag="div">
-  //       {String(children).replace(/\n$/, '')}
-  //     </SyntaxHighlighter>
-  //   ) : (
-  //     <code className={className} {...props}>
-  //       {children}
-  //     </code>
-  //   )
-  // }
-
-  const Table: React.FC = ({ children }) => {
-    return (
-      <div className="flex overflow-auto">
-        <table className="table-auto">{children}</table>
-      </div>
-    )
-  }
-
   return (
     <Fragment>
       <div className="markdown text-gray-500 dark:text-gray-300 mb-10">
@@ -84,10 +34,34 @@ const PostViewContent: React.FC<PostViewContentProps> = ({ content }) => {
                 </code>
               )
             },
-            h1: H1,
-            h2: H2,
-            h3: H3,
-            table: Table,
+            h1({ children }) {
+              return (
+                <Title type="h1" className="text-center mb-4">
+                  {children}
+                </Title>
+              )
+            },
+            h2({ children }) {
+              return (
+                <Title type="h2" className="mb-2 mt-6">
+                  {children}
+                </Title>
+              )
+            },
+            h3({ children }) {
+              return (
+                <Title type="h3" className="mb-2 mt-4">
+                  {children}
+                </Title>
+              )
+            },
+            table({ children }) {
+              return (
+                <div className="flex overflow-auto">
+                  <table className="table-auto">{children}</table>
+                </div>
+              )
+            },
           }}
           remarkPlugins={[remarkGfm, remarkMath]}
           rehypePlugins={[rehypeKatex]}
