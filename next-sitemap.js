90f929243b92821cf379fb72ff2f71078c283315
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2021-12-03 01:29:17 +0900

    fix: sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: 'https://wisteken.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  exclude: ['/server-sitemap.xml'], // <= exclude here
+  exclude: ['/search', '/server-sitemap.xml'], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
       'https://wisteken.com/server-sitemap.xml', // <==== Add here
